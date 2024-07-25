6a34b5bb7e4bd5bd94ae015397ea3f6d094f846e
Keyur Bhalodiya | KB <kbit920@outlook.com>
2024-07-25 13:22:23 +0530

    Initial Commit

---
+++ StockPriceApp/StockPriceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockPriceAppApp.swift
+//  StockPriceApp
+//
+//  Created by Keyur Bhalodiya on 2024/07/25.
+//
+
+import SwiftUI
+
+@main
+struct StockPriceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StockPriceAppUITests/StockPriceAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StockPriceAppUITestsLaunchTests.swift
+//  StockPriceAppUITests
+//
+//  Created by Keyur Bhalodiya on 2024/07/25.
+//
+
+import XCTest
+
+final class StockPriceAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
